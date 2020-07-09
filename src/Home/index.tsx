aa5b5a04298f26d8317ab0fa318527a02520e5e2
Silas Almeida <silasadssn@gmail.com>
2020-07-09 02:30:36 -0300

    add: current position

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Alert } from 'react-native';
+
 import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
 
 import styles from './styles';
 
 const Map: React.FC = () => {
+  const [location, setLocation] = useState<Location.LocationData>();
+
+  const handleCurrentPosition = async () => {
+    let { status } = await Location.requestPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert('Permission to access location was denied');
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+  };
+
+  useEffect(() => {
+    handleCurrentPosition();
+  });
+
   return (
     <View style={styles.container}>
       <MapView style={styles.mapView} />
